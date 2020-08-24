71ef2087a7265a550498f5dc52797426809d63c8
Shjani49 <shjani49@gmail.com>
2020-08-24 10:14:56 -0600

    Initial Commit

---
+++ CSharp_OOP_Prac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_OOP_Prac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
