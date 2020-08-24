f2be871c2f100cbdf59e1cb0e93a7d16eaf359d4
Shjani49 <shjani49@gmail.com>
2020-08-24 10:25:30 -0600

    ternary operator   Example

--- CSharp_OOP_Prac/Program.cs
+++ CSharp_OOP_Prac/Program.cs
@@ -6,7 +6,19 @@ namespace CSharp_OOP_Prac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // If an input string contains a space, output "Multiple words.", otherwise output "One word."
+            // Make sure the input is trimmed before checking. Try to do it with one variable for input and one with output.
+            // Use a ternary operator. If you finish that early, Try to do it without any variables at all (remove the line above 
+            // these comments).
+
+            string input, output;
+            Console.WriteLine("Please Eneter a String: ");
+            input = Console.ReadLine();
+
+            output = input.Trim().Contains(" ") ? "Multiple Words" : "One word";
+            Console.WriteLine(output);
+
         }
     }
 }
