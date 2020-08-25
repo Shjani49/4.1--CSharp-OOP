2f4074e99868b7a621ca603b1079174ccf0e357f
Shjani49 <shjani49@gmail.com>
2020-08-25 10:15:57 -0600

    Added extensive comments and a second test case;

--- CSharp_OOP_Prac/Program.cs
+++ CSharp_OOP_Prac/Program.cs
@@ -59,9 +59,44 @@ namespace CSharp_OOP_Prac
 
             Console.WriteLine($"The class has the classcode {theClass.ClassCode}, is called {theClass.ClassName} and is being taught by {theClass.ClassTeacher.FirstName} {theClass.ClassTeacher.LastName}.");
 
+            SchoolClass historyClass = new SchoolClass();
+            historyClass.ClassCode = "HIST101";
+            historyClass.ClassName = "Introduction to History";
 
+            // Rather than assigning props as multiple statements, you can assign them during construction.
+            SchoolClass englishClass = new SchoolClass()
+            {
+                ClassCode = "ENGL101",
+                ClassName = "Introduction to English"
+            };
 
-            
+            Teacher sueTeacher = new Teacher()
+            {
+                StaffID = 2050,
+                FirstName = "Sue",
+                LastName = "Smith"
+            };
+            Teacher joeTeacher = new Teacher()
+            {
+                StaffID = 2050,
+                FirstName = "Joe",
+                LastName = "Testificate"
+            };
+
+            // For this test run:
+            // Old teacher will be NULL
+            // New teacher will be Sue
+            // "this" will be History class
+            historyClass.ClassTeacher = sueTeacher;
+
+            // For this test run:
+            // Old teacher will be Sue
+            // New teacher will be Joe
+            // "this" will be History class
+            historyClass.ClassTeacher = joeTeacher;
+
+            Console.WriteLine($"The class has the classcode {historyClass.ClassCode}, is called {historyClass.ClassName} and is being taught by {historyClass.ClassTeacher.FirstName} {historyClass.ClassTeacher.LastName}.");
+
 
         }
     }
